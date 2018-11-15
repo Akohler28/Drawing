f5713ce7c03e2159f4dadf6b79c529120cb0fa2b
Aaron <aaronakohler@gmail.com>
2018-11-15 15:56:57 -0800

    Added lines, shapes, and images

--- app/src/main/java/edu/cascadia/brianb/mydrawing/SimplyDrawnView.java
+++ app/src/main/java/edu/cascadia/brianb/mydrawing/SimplyDrawnView.java
@@ -10,6 +10,7 @@ import android.graphics.Path;
 import android.graphics.PathDashPathEffect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -43,23 +44,47 @@ public class SimplyDrawnView extends View {
         mPaint.setStrokeWidth(16.0f);
         canvas.drawLine(mWidth, 0, 200, mHeight, mPaint);
 
-        //TODO: Finish drawing red line
-        mPaint.setColor(Color.RED); mPaint.setStrokeWidth(10);
+        //TODOne: Finish drawing red line
+        mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth(10);
+        canvas.drawLine(mWidth, 5, 100, mHeight, mPaint);
 
-        //TODO: Draw green lines
+        //TODOne: Draw green lines
+        mPaint.setColor(Color.GREEN);
+        canvas.drawLine(mWidth, 50, 200, mHeight, mPaint);
 
-        //TODO: Draw Text
+        //TODOne: Draw Text
+        mPaint.setStrokeWidth(50);
+        mPaint.setColor(Color.BLACK);
+        canvas.drawText("Hello there!", 20, 20, mPaint);
 
-        //TODO: Draw Text on a Path
+        //TODOne: Draw Text on a Path
+        Path path = new Path();
+        mPaint.setColor(Color.BLUE);
+        path.addCircle(40, 90, 100, Path.Direction.CW);
+        canvas.drawTextOnPath("This one is on a path!", path, 0, 0, mPaint);
 
-        //TODO: Draw filled, opaque, and open ovals
+        //TODOne: Draw filled, opaque, and open ovals
+        canvas.drawOval(mWidth, 0, mWidth - 50, 50, mPaint);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(5);
+        canvas.drawOval(100, 200, 200, 400, mPaint);
+        mPaint.setAlpha(20);
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawOval(400, 400, 500, 600, mPaint);
 
-        //TODO: Draw bee bitmap
+
+        //TODOne: Draw bee bitmap
         //     HINT: since b is a Drawable, you can use its .draw method, but .setBounds on it first
         Drawable b = getResources().getDrawable(R.drawable.bee,null);
+        b.setBounds(10, 600,  100, 750);
+        b.draw(canvas);
 
-        //TODO: Add another image to the project (copy and paste to Android Studio res/drawable folder)
+        //TODOne: Add another image to the project (copy and paste to Android Studio res/drawable folder)
         //     and draw it on the screen
+        Drawable d = getResources().getDrawable(R.drawable.duck);
+        d.setBounds(10, 775, 100, 900);
+        d.draw(canvas);
 
     }
 }
